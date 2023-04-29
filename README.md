ce57b6d731c855580d7e6cc7d729157b5dc9cd4d
DorianRec <dorian.rec@gmail.com>
2023-04-29 23:07:24 +0200

    * Fixed some buggy string-convertions. * Added Gdiplus as dependency in the CMake file. * Added usage prompt. * Added the possibility to specify target-folder, width and height.

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # WMFConverter
 
-A program to <b>convert</b> '.wmf' files to '.png' files and adjust <b>width</b> and <b>height</b> to pixels.
+A program to <b>convert</b> WMF files to PNG files and adjust <b>width</b> and <b>height</b> to pixels.
 
-Dependencies: <b>Windows API</b>
+Usage: WMFConverter.exe target-folder width height
+
+The converter recursively loops through the target-folder and converts all WMF files into PNG format.
+The program creates a folder 'PNG' in which the new PNG files are stored in a similar folder-structure as they are found in the target-folder.
+The WMF files will get resized to the given width and height.
+
+Dependencies:
+* <b>Windows API</b>
+* <b>GDI+</b>
 
 Tested on:
-* OS: Windows 10 Pro 64-Bit
-* Toolchain: Microsoft Visual Studio 2019 Community (Version 16.0)
-* CMake Version 3.19.2
-* Cpp 20
+* OS: Windows 11 Pro x64
+* C++ 20
+* Compiler: Cygwin64 v3.4.3
+* CMake Version 3.22
--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <stdio.h>
 
 using namespace Gdiplus;
-#pragma comment (lib, "Gdiplus.lib")
 
 #include <iostream>
 #include <filesystem>
@@ -16,25 +15,29 @@ namespace fs = std::filesystem;
  * @param format
  * @param pClsid
  * @return
+ * Source: [1]
  */
-int GetEncoderClsid(const WCHAR *format, CLSID *pClsid) {
-    UINT num = 0;          // number of image encoders
-    UINT size = 0;         // size of the image encoder array in bytes
+int GetEncoderClsid(const WCHAR* format, CLSID* pClsid)
+{
+    UINT  num = 0;          // number of image encoders
+    UINT  size = 0;         // size of the image encoder array in bytes
 
-    ImageCodecInfo *pImageCodecInfo = NULL;
+    ImageCodecInfo* pImageCodecInfo = NULL;
 
     GetImageEncodersSize(&num, &size);
-    if (size == 0)
+    if(size == 0)
         return -1;  // Failure
 
-    pImageCodecInfo = (ImageCodecInfo *) (malloc(size));
-    if (pImageCodecInfo == NULL)
+    pImageCodecInfo = (ImageCodecInfo*)(malloc(size));
+    if(pImageCodecInfo == NULL)
         return -1;  // Failure
 
     GetImageEncoders(num, size, pImageCodecInfo);
 
-    for (UINT j = 0; j < num; ++j) {
-        if (wcscmp(pImageCodecInfo[j].MimeType, format) == 0) {
+    for(UINT j = 0; j < num; ++j)
+    {
+        if( wcscmp(pImageCodecInfo[j].MimeType, format) == 0 )
+        {
             *pClsid = pImageCodecInfo[j].Clsid;
             free(pImageCodecInfo);
             return j;  // Success
@@ -50,9 +53,9 @@ int GetEncoderClsid(const WCHAR *format, CLSID *pClsid) {
  *
  * @param inputPath The location of the '.wmf' file
  * @return 0
- * Source: [1] [2]
+ * Source: [3] [2]
  */
-int convert(const fs::path *inputPath) {
+int convert(const wchar_t* filename, int width, int height) {
 
     // Initialize GDI+.
     GdiplusStartupInput gdiplusStartupInput;
@@ -61,23 +64,21 @@ int convert(const fs::path *inputPath) {
 
     CLSID encoderClsid;
     Status stat;
-    Image *image = new Image(inputPath->c_str());
+    Image *image = new Image(filename);
 
     // Get the CLSID of the PNG encoder.
-    GetEncoderClsid(L"image/png", &encoderClsid); // TODO: Wished output format
+    GetEncoderClsid(L"image/png", &encoderClsid); // Wished output format
 
     // Generate Bitmap to print to
-    int width = 1900; // TODO: Wished output width [in pixels]
-    int height = 1472; // TODO: Wished output height [in pixels]
     Bitmap *target = new Bitmap(width, height);
     Graphics *g = Graphics::FromImage(target);
     g->DrawImage(image, 0, 0, width, height);
     // end
 
     // Build output inputPath
-    fs::path outputPath = fs::path("PNGs"); // TODO: Wished output folder
-    outputPath /= inputPath->string();
-    outputPath.replace_extension(".png"); // TODO: Wished output format
+    fs::path outputPath = fs::path("PNGs"); // Wished output folder
+    outputPath /= filename;
+    outputPath.replace_extension(".png"); // Wished output format
 
     // Checks, whether target folders exists (and creates inputPath)
     fs::path directories = fs::path(outputPath);
@@ -87,7 +88,7 @@ int convert(const fs::path *inputPath) {
     }
 
     // Creates or overwrites file!
-    stat = target->Save(outputPath.c_str(), &encoderClsid, NULL);
+    stat = target->Save(outputPath.wstring().c_str(), &encoderClsid, NULL);
 
     if (stat == Ok)
         printf("%s was saved successfully at\n%s\n",
@@ -107,22 +108,41 @@ int convert(const fs::path *inputPath) {
     return 0;
 }
 
-int main() {
-    std::string path(".");
+int main(int argc, char* argv[]) {
+    // START: Validate the input
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " target-folder width height" << std::endl;
+        return 1; // error code
+    }
+
+    std::string targetFolder = argv[1];
+    int width = std::stoi(argv[2]); // 1900
+    int height = std::stoi(argv[3]); // 1472
+
+    std::cout << "The input string is: " << targetFolder << std::endl;
+    std::cout << "The width is: " << width << std::endl;
+    std::cout << "The height is: " << height << std::endl;
+    // END: Validate the input
+
+    std::string path(targetFolder);
     // Only convert files with 'wmf' extension
-    std::string ext(".wmf"); // TODO: Wished input format
+    std::string ext(".wmf"); // Wished input format
 
     // Loop recursively through folders and recognize all files with given file extension
-    for (auto &p : fs::recursive_directory_iterator(path)) {
+    for (const auto& p : fs::recursive_directory_iterator(path)) {
         if (p.path().extension() == ext) {
-            convert(&p.path());
+            std::wstring filePathW = p.path().wstring();
+            const wchar_t* filePathWC = filePathW.c_str();
+
+            convert(filePathWC, width, height);
+            std::cout << p.path().string() << std::endl;
         }
     }
 
-    return 0;
+    return 0; // success
 }
 
 // Sources:
 // [1] https://docs.microsoft.com/en-us/windows/win32/gdiplus/-gdiplus-retrieving-the-class-identifier-for-an-encoder-use
 // [2] https://stackoverflow.com/questions/12098924/how-do-i-resize-shrink-an-emf-metafile-in-net
-
+// [3] https://learn.microsoft.com/en-us/windows/win32/gdiplus/-gdiplus-converting-a-bmp-image-to-a-png-image-use
